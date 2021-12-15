5e015219b1dbab1d63411b7297c8e0c9f7309ca8
sampoo2 <93064268+sampoo2@users.noreply.github.com>
2021-12-15 19:11:57 +0900

    123

--- cloud/admin/Thumbnail_ok.php
+++ cloud/admin/Thumbnail_ok.php
@@ -1,13 +1,13 @@
 <?php 
-//session_start();
+session_start();
 
-//$regist_day = date("Y-m-d (H:i)");
-//include "../../login/dbconn.php";
+$regist_day = date("Y-m-d (H:i)");
+include "../../login/dbconn.php";
 
 //$userid = $_SESSION['userid'];
-//$FILE_SEQ = $_POST['FILE_SEQ'];
-//$FILE_GROUP = $_POST['FILE_GROUP'];
-//$FILE_TEXT = $_POST['FILE_TEXT'];
+$FILE_SEQ = $_POST['FILE_SEQ'];
+$FILE_GROUP = $_POST['FILE_GROUP'];
+$FILE_TEXT = $_POST['FILE_TEXT'];
 
 if(is_uploaded_file($_FILES['upfile']['tmp_name']))
 {
@@ -16,12 +16,12 @@ if(is_uploaded_file($_FILES['upfile']['tmp_name']))
     $file_name = $_FILES['upfile']['name'];
 }
 
-//$sql = "insert into greet (FILE_NAME, FILE_SEQ, FILE_GROUP, FILE_TEXT, FILE_ADMIN, REGIST_DAY)";
-//$sql .= "values ('$FILE_NAME', '$FILE_SEQ', '$FILE_GROUP', '$FILE_TEXT', '$FILE_ADMIN','$REGIST_DAY')";
+$sql = "insert into Thumbnail (FILE_NAME, FILE_SEQ, FILE_GROUP, FILE_TEXT, FILE_ADMIN, REGIST_DAY)";
+$sql .= "values ('$FILE_NAME', '$FILE_SEQ', '$FILE_GROUP', '$FILE_TEXT', '$FILE_ADMIN','$REGIST_DAY')";
 
-echo '<script>'; 
-echo 'alert("FILE_NAME : '.$FILE_NAME .'");'; 
-echo '</script>';
+//echo '<script>'; 
+//echo 'alert("FILE_NAME : '.$FILE_NAME .'");'; 
+//echo '</script>';
 
 //mysqli_query($connect, $sql);
 //mysqli_close();

7b0952c394a703a1c4ae111f2084096ecae09455
sampoo2 <93064268+sampoo2@users.noreply.github.com>
2021-12-15 19:22:31 +0900

    123

--- cloud/admin/Thumbnail_ok_bak.php
+++ cloud/admin/Thumbnail_ok_bak.php
@@ -1,6 +1,6 @@
 <?php
 // 설정
-$uploads_dir = "/var/www/html/cloud/admin/bak";;
+$uploads_dir = "/var/www/html/cloud/admin/bak";
 $allowed_ext = array('jpg','jpeg','png','gif');
 $value = '업로드 완료되었습니다.';
 

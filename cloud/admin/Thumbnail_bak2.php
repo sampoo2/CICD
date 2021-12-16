ef545676e457219f228dc6f2d1f42299355832a4
sampoo2 <93064268+sampoo2@users.noreply.github.com>
2021-12-16 12:49:58 +0900

    123

--- cloud/admin/Thumbnail_bak2.php
+++ cloud/admin/Thumbnail_bak2.php
@@ -157,11 +157,11 @@
                                 <label>Custom File upload</label>
                                 <div class="input-group">
                                 <div class="input-group-prepend">
-                                    <span class="input-group-text">Upload2</span>
+                                    <span class="input-group-text">Upload</span>
                                 </div>
                                 <div class="custom-file">
                                     <!--<input type="file" name="upfile" class="form-control" id="upfile">-->
-                                    <input type="file" name="FILE_NAME">
+                                    <input type="text" name="FILE_NAME" class="custom-file-input">
                                     <!--<label class="custom-file-label" for="upfile">Choose file</label>-->
                                 </div>
                                 </div>

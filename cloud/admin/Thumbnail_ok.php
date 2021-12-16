a493e616b2b2c6e8850682533e872953e8fb1942
sampoo2 <93064268+sampoo2@users.noreply.github.com>
2021-12-16 11:35:06 +0900

    123

--- cloud/admin/Thumbnail_ok.php
+++ cloud/admin/Thumbnail_ok.php
@@ -13,25 +13,25 @@ $FILE_ADMIN = $_POST['FILE_ADMIN'];
 
 if(is_uploaded_file($_FILES['upfile']['tmp_name']))
 {
-    $destination = "../data/" . $_FILES['upfile']['name'];
-    move_uploaded_file($_FILES['upfile']['tmp_name'], $destination);
-    $file_name = $_FILES['upfile']['name'];
+	$destination = "../data/" . $_FILES['upfile']['name'];
+	move_uploaded_file($_FILES['upfile']['tmp_name'], $destination);
+	$file_name = $_FILES['upfile']['name'];
 }
 
 $sql = "insert into Thumbnail (FILE_NAME, FILE_SEQ, FILE_GROUP, FILE_TEXT, FILE_ADMIN, REGIST_DAY)";
 $sql .= "values ('$FILE_NAME', '$FILE_SEQ', '$FILE_GROUP', '$FILE_TEXT', 'admin','$REGIST_DAY')";
 
-//mysqli_query($connect, $sql);
-//mysqli_close();
+mysqli_query($connect, $sql);
+mysqli_close();
 
-echo "<h2>파일 정보</h2>
-<ul>
-	<li>sql: $sql</li>
-	<li>regist_day: $REGIST_DAY</li>
-	<li>FILE_NAME: $FILE_NAME</li>
-	<li>파일형식: {$_FILES['myfile']['type']}</li>
-	<li>파일크기: {$_FILES['myfile']['size']} 바이트</li>
-</ul>";
+//echo "<h2>파일 정보</h2>
+//<ul>
+//	<li>sql: $sql</li>
+//	<li>regist_day: $REGIST_DAY</li>
+//	<li>FILE_NAME: $FILE_NAME</li>
+//	<li>파일형식: {$_FILES['myfile']['type']}</li>
+//	<li>파일크기: {$_FILES['myfile']['size']} 바이트</li>
+//</ul>";
 
 ?>
 

4a9740c80090f4905c42dec32164a16f925a7903
sampoo2 <93064268+sampoo2@users.noreply.github.com>
2021-12-16 17:41:38 +0900

    asdf

--- cloud/admin/Video_ok.php
+++ cloud/admin/Video_ok.php
@@ -17,16 +17,16 @@ $VIDEO_GROUP = $_POST['VIDEO_GROUP'];
 $sql = "insert into Video (VIDEO_SEQ , VIDEO_GROUP , VIDEO_NAME , VIDEO_SUBJECT , VIDEO_FILE , VIDEO_ADMIN , REGIST_DAY)";
 $sql .= "values ('$VIDEO_SEQ ', '$VIDEO_GROUP ', '$VIDEO_NAME ', '$VIDEO_SUBJECT ', '$VIDEO_FILE ','admin','$REGIST_DAY')";
 	
-//mysqli_query($connect, $sql);
-//mysqli_close();
+mysqli_query($connect, $sql);
+mysqli_close();
 	
-echo "<h2>파일 정보</h2>
-<ul>
-<li>sql: $sql</li>
-<li>VIDEO_GROUP: $VIDEO_GROUP</li>
-</ul>";	
-//echo "<script>alert('{$value}');</script>";
-//echo "<script>location.href='./Video.php'</script>";
+//echo "<h2>파일 정보</h2>
+//<ul>
+//<li>sql: $sql</li>
+//<li>VIDEO_GROUP: $VIDEO_GROUP</li>
+//</ul>";	
+echo "<script>alert('{$value}');</script>";
+echo "<script>location.href='./Video.php'</script>";
 ?>
 
 

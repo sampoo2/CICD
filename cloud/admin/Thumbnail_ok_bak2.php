00aa26c133bacd2278c1ca2838acb2614a2a93e6
sampoo2 <93064268+sampoo2@users.noreply.github.com>
2021-12-16 12:26:59 +0900

    123

--- cloud/admin/Thumbnail_ok_bak2.php
+++ cloud/admin/Thumbnail_ok_bak2.php
@@ -1,10 +1,22 @@
 <?php
+
+session_start();
+
+$REGIST_DAY = date("Y-m-d (H:i)");
+include "../login/dbconn.php";
+
 	if(is_uploaded_file($_FILES['upfile']['tmp_name']))
 	{
 		$destination = "../data/" . $_FILES['upfile']['name'];
 		move_uploaded_file($_FILES['upfile']['tmp_name'], $destination);
 		$file_name = $_FILES['upfile']['name'];
 	}
+
+	$FILE_SEQ = $_POST['FILE_SEQ'];
+	$FILE_GROUP = $_POST['FILE_GROUP'];
+	$FILE_TEXT = $_POST['FILE_TEXT'];
+	$FILE_NAME = $_POST['upfile'];
+	$FILE_ADMIN = $_POST['FILE_ADMIN'];	
 ?>
 
 

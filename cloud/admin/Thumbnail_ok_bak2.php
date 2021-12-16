198d41bd1fed70354d8418959250bbd92fa65361
sampoo2 <93064268+sampoo2@users.noreply.github.com>
2021-12-16 12:35:29 +0900

    123

--- cloud/admin/Thumbnail_ok_bak2.php
+++ cloud/admin/Thumbnail_ok_bak2.php
@@ -5,6 +5,12 @@ session_start();
 $REGIST_DAY = date("Y-m-d (H:i)");
 include "../login/dbconn.php";
 
+$FILE_SEQ = $_POST['FILE_SEQ'];
+$FILE_GROUP = $_POST['FILE_GROUP'];
+$FILE_TEXT = $_POST['FILE_TEXT'];
+$FILE_NAMES = $_POST['upfile'];
+$FILE_ADMIN = $_POST['FILE_ADMIN'];	
+
 	if(is_uploaded_file($_FILES['upfile']['tmp_name']))
 	{
 		$destination = "../data/" . $_FILES['upfile']['name'];
@@ -12,11 +18,7 @@ include "../login/dbconn.php";
 		$file_name = $_FILES['upfile']['name'];
 	}
 
-	$FILE_SEQ = $_POST['FILE_SEQ'];
-	$FILE_GROUP = $_POST['FILE_GROUP'];
-	$FILE_TEXT = $_POST['FILE_TEXT'];
-	$FILE_NAMES = $_POST['upfile'];
-	$FILE_ADMIN = $_POST['FILE_ADMIN'];	
+
 
 	$sql = "insert into Thumbnail (FILE_NAME, FILE_SEQ, FILE_GROUP, FILE_TEXT, FILE_ADMIN, REGIST_DAY)";
 	$sql .= "values ('$FILE_NAMES', '$FILE_SEQ', '$FILE_GROUP', '$FILE_TEXT', 'admin','$REGIST_DAY')";

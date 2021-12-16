b0fd4909598d335ca0f5e3d30cb40445e14bba4f
sampoo2 <93064268+sampoo2@users.noreply.github.com>
2021-12-16 12:33:13 +0900

    123

--- cloud/admin/Thumbnail_ok_bak2.php
+++ cloud/admin/Thumbnail_ok_bak2.php
@@ -15,11 +15,11 @@ include "../login/dbconn.php";
 	$FILE_SEQ = $_POST['FILE_SEQ'];
 	$FILE_GROUP = $_POST['FILE_GROUP'];
 	$FILE_TEXT = $_POST['FILE_TEXT'];
-	$FILE_NAME = $_POST['upfile'];
+	$FILE_NAMES = $_POST['upfile'];
 	$FILE_ADMIN = $_POST['FILE_ADMIN'];	
 
 	$sql = "insert into Thumbnail (FILE_NAME, FILE_SEQ, FILE_GROUP, FILE_TEXT, FILE_ADMIN, REGIST_DAY)";
-	$sql .= "values ('$FILE_NAME', '$FILE_SEQ', '$FILE_GROUP', '$FILE_TEXT', 'admin','$REGIST_DAY')";
+	$sql .= "values ('$FILE_NAMES', '$FILE_SEQ', '$FILE_GROUP', '$FILE_TEXT', 'admin','$REGIST_DAY')";
 	
 	mysqli_query($connect, $sql);
 	mysqli_close();

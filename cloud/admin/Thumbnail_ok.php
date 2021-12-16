4ba73af8a74643860310ecc289538a6fa4e2c346
sampoo2 <93064268+sampoo2@users.noreply.github.com>
2021-12-16 11:23:20 +0900

    123

--- cloud/admin/Thumbnail_ok.php
+++ cloud/admin/Thumbnail_ok.php
@@ -8,10 +8,11 @@ include "../../login/dbconn.php";
 $FILE_SEQ = $_POST['FILE_SEQ'];
 $FILE_GROUP = $_POST['FILE_GROUP'];
 $FILE_TEXT = $_POST['FILE_TEXT'];
+$FILE_NAME = $_POST['upfile'];
 
 if(is_uploaded_file($_FILES['upfile']['tmp_name']))
 {
-    $destination = "./" . $_FILES['upfile']['name'];
+    $destination = "../data/" . $_FILES['upfile']['name'];
     move_uploaded_file($_FILES['upfile']['tmp_name'], $destination);
     $file_name = $_FILES['upfile']['name'];
 }
@@ -25,7 +26,8 @@ $sql .= "values ('$FILE_NAME', '$FILE_SEQ', '$FILE_GROUP', '$FILE_TEXT', '$FILE_
 echo "<h2>파일 정보</h2>
 <ul>
 	<li>sql: $sql</li>
-	<li>확장자: $ext</li>
+	<li>regist_day: $regist_day</li>
+	<li>FILE_NAME: $FILE_NAME</li>
 	<li>파일형식: {$_FILES['myfile']['type']}</li>
 	<li>파일크기: {$_FILES['myfile']['size']} 바이트</li>
 </ul>";

64bb97f752a7877db16b35bb83816a643e7ac435
sampoo2 <93064268+sampoo2@users.noreply.github.com>
2021-12-16 10:34:22 +0900

    123

--- cloud/admin/Thumbnail_ok_bak2.php
+++ cloud/admin/Thumbnail_ok_bak2.php
@@ -1,8 +1,10 @@
 <?php
 	if(is_uploaded_file($_FILES['upfile']['tmp_name']))
 	{
-		$destination = "./bak/" . $_FILES['upfile']['name'];
+		$destination = "/var/www/html/cloud/admin/bak" . $_FILES['upfile']['name'];
 		move_uploaded_file($_FILES['upfile']['tmp_name'], $destination);
 		$file_name = $_FILES['upfile']['name'];
 	}
 ?>
+
+
